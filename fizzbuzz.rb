572dd1abdb3a81d17504d09fb76200b1e27d7523
Thomas John Helin <thomashelin@yahoo.com>
2020-03-17 20:27:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-
+ if number % 3 == 0
+   "Fizz"
+ end
+ if number % 5 == 0
+   "Buzz"
+ end
+ if number % 3 == 0 && number % 5 == 0
+   "FizzBuzz"
+ end
 end 
