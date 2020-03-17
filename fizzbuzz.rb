2b1391af97455937508132ccb4621de815fae393
Thomas John Helin <thomashelin@yahoo.com>
2020-03-17 20:37:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
  if number % 3 == 0 && number % 5 == 0
- "FizzBuzz"
+ return "FizzBuzz"
  end
  if number % 3 == 0
  return "Fizz"
